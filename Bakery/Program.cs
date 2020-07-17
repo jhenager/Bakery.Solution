f7a03d9e51bf2425ba9c9cece8ec85b902d9d104
James Henager <jameschenager@gmail.com>
2020-07-17 15:16:23 -0700

    add first bit of ui that gathers input for bread

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,14 @@ namespace Bakery
   {
     public static void Main()
     {
-      //Code goes here!
+      Console.WriteLine("Welcome to Henager Bakery!");
+      Console.WriteLine("We offer Bread and Pastries.");
+      Console.WriteLine("Bread is $5 a loaf. Our special for Bread is buy 2 get 1 free.");
+      Console.WriteLine("Pastries are $2 each. Our special for Pastries is buy 2 get 1 half off.");
+      Console.WriteLine("How many loaves of bread would you like?");
+      int breadInput = int.Parse(Console.ReadLine());
+      Bread bread1 = new Bread(breadInput);
+      Console.WriteLine(bread1.BreadTotal());
     }
   }
 }
