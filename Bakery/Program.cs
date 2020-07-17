ec74d01b2b1d3153e5bc63b7c7ddd1c0398a5794
James Henager <jameschenager@gmail.com>
2020-07-17 16:21:02 -0700

    add ui method to check if user input is a number or not

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,13 +12,39 @@ namespace Bakery
       Console.WriteLine("Bread is $5 a loaf. Our special is buy 2 get 1 free.");
       Console.WriteLine("Pastries are $2 each. Our special is buy 2 get 1 half off.");
       Console.WriteLine("How many loaves of bread would you like?");
-      int breadInput = int.Parse(Console.ReadLine());
-      Bread bread1 = new Bread(breadInput);
+      string breadInput = Console.ReadLine();
+
+      if (numberChecker(breadInput) == false)
+      {
+        Console.WriteLine("Please enter a number with no spaces or extra characters");
+        Main();
+      }
+      
+      Bread bread1 = new Bread(int.Parse(breadInput));
       Console.WriteLine("How many pastries would you like?");
-      int pastryInput = int.Parse(Console.ReadLine());
-      Pastry pastry1 = new Pastry(pastryInput);
+      string pastryInput = Console.ReadLine();
+
+      if (numberChecker(pastryInput)== false)
+      {
+        Console.WriteLine("Please enter a number with no spaces or extra characters");
+        Main();
+      }
+
+      Pastry pastry1 = new Pastry(int.Parse(pastryInput));
       Order order1 = new Order(bread1.BreadTotal(), pastry1.PastryTotal());
-      Console.WriteLine("Thank you. For " + breadInput + " loaves of bread and " + pastryInput + " pastries, your total is " + order1.OrderTotal());
+      Console.WriteLine("Thank you. For " + breadInput + " loaves of bread and " + pastryInput + " pastries, your total is $" + order1.OrderTotal());
+    }
+
+    public static bool numberChecker(string input)
+    { 
+      foreach(char number in input)
+      {
+        if (char.IsNumber(number) != true)
+        {
+          return false;
+        }
+      }
+        return true;
     }
   }
 }
