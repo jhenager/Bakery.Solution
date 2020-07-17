d0082eb2b09f1215639a33d99e9a462ac88c3830
James Henager <jameschenager@gmail.com>
2020-07-17 15:48:29 -0700

    add ui logic to display user total

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,15 +9,16 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Henager Bakery!");
       Console.WriteLine("We offer Bread and Pastries.");
-      Console.WriteLine("Bread is $5 a loaf. Our special for Bread is buy 2 get 1 free.");
-      Console.WriteLine("Pastries are $2 each. Our special for Pastries is buy 2 get 1 half off.");
+      Console.WriteLine("Bread is $5 a loaf. Our special is buy 2 get 1 free.");
+      Console.WriteLine("Pastries are $2 each. Our special is buy 2 get 1 half off.");
       Console.WriteLine("How many loaves of bread would you like?");
       int breadInput = int.Parse(Console.ReadLine());
       Bread bread1 = new Bread(breadInput);
       Console.WriteLine("How many pastries would you like?");
       int pastryInput = int.Parse(Console.ReadLine());
       Pastry pastry1 = new Pastry(pastryInput);
-      Console.WriteLine(pastry1.PastryTotal());
+      Order order1 = new Order(bread1.BreadTotal(), pastry1.PastryTotal());
+      Console.WriteLine("Thank you. For " + breadInput + " loaves of bread and " + pastryInput + " pastries, your total is " + order1.OrderTotal());
     }
   }
 }
