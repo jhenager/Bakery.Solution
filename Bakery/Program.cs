183f182fa2b2947dfb72021c8a066d0d3711a448
James Henager <jameschenager@gmail.com>
2020-07-17 16:44:59 -0700

    add ui logic to restart method if an input is not a number

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,26 +11,32 @@ namespace Bakery
       Console.WriteLine("We offer Bread and Pastries.");
       Console.WriteLine("Bread is $5 a loaf. Our special is buy 2 get 1 free.");
       Console.WriteLine("Pastries are $2 each. Our special is buy 2 get 1 half off.");
+      Start();
+    }
+
+    public static void Start()
+    {
       Console.WriteLine("How many loaves of bread would you like?");
       string breadInput = Console.ReadLine();
 
       if (numberChecker(breadInput) == false)
       {
         Console.WriteLine("Please enter a number with no spaces or extra characters");
-        Main();
+        Start();
       }
       
-      Bread bread1 = new Bread(int.Parse(breadInput));
       Console.WriteLine("How many pastries would you like?");
       string pastryInput = Console.ReadLine();
 
       if (numberChecker(pastryInput)== false)
       {
         Console.WriteLine("Please enter a number with no spaces or extra characters");
-        Main();
+        Start();
       }
-
-      Pastry pastry1 = new Pastry(int.Parse(pastryInput));
+      int parsedPastry = int.Parse(pastryInput);
+      int parsedBread = int.Parse(breadInput);
+      Bread bread1 = new Bread(parsedBread);
+      Pastry pastry1 = new Pastry(parsedPastry);
       Order order1 = new Order(bread1.BreadTotal(), pastry1.PastryTotal());
       Console.WriteLine("Thank you. For " + breadInput + " loaves of bread and " + pastryInput + " pastries, your total is $" + order1.OrderTotal());
     }
@@ -45,6 +51,6 @@ namespace Bakery
         }
       }
         return true;
-    }
+    }  
   }
 }
