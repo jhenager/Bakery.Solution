2311e8d87c9c6ae27c8d65bd2ff430552736b84c
James Henager <jameschenager@gmail.com>
2020-07-17 09:22:22 -0700

    add passing test for creating an instance of bread object

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreateInstanceOfBread_Bread()
+    {
+      Bread bread1 = new Bread();
+      Assert.AreEqual(typeof(Bread), bread1.GetType());
+    }
+  }
+}
